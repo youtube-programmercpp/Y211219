61687ba4843d1e2e5e60ac522846c0ca52feb4af
ProgrammerCpp <sbl908k@yahoo.co.jp>
2022-12-07 19:14:10 +0900

    1. IMAGE_DATA_DIRECTORY::Size == 0 の場合を考慮 2. IMAGE_EXPORT_DIRECTORY::AddressOfNameOrdinalsの参照漏れバグを修正

--- q12271411197_dec2bin/ExportedFunctionsFromModule.cpp
+++ q12271411197_dec2bin/ExportedFunctionsFromModule.cpp
@@ -6,16 +6,22 @@
 std::vector<ExportedFunction> ExportedFunctionsFromModule(const IMAGE_DOS_HEADER& imageDosHeader)
 {
 	std::vector<ExportedFunction> retval;
-	const auto& d = *PIMAGE_EXPORT_DIRECTORY(&LPCSTR(&imageDosHeader)[PIMAGE_NT_HEADERS(&LPCSTR(&imageDosHeader)[imageDosHeader.e_lfanew])->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress]);
+	const auto&dd = PIMAGE_NT_HEADERS(&LPCSTR(&imageDosHeader)[imageDosHeader.e_lfanew])->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+	if (dd.Size) {
+	const auto& d = *PIMAGE_EXPORT_DIRECTORY(&LPCSTR(&imageDosHeader)[dd.VirtualAddress]);
 	const auto AddressOfFunctions = reinterpret_cast<const DWORD*>(&LPCSTR(&imageDosHeader)[d.AddressOfFunctions]);
 	const auto AddressOfNames     = reinterpret_cast<const DWORD*>(&LPCSTR(&imageDosHeader)[d.AddressOfNames    ]);
+	const auto AddressOfNameOrdinals = reinterpret_cast<const WORD*>(&LPCSTR(&imageDosHeader)[d.AddressOfNameOrdinals]);
 	retval.reserve(d.NumberOfNames);
 	for (DWORD i = 0; i < d.NumberOfNames; ++i) {
 		retval.push_back(
-		{ &LPCSTR(&imageDosHeader)[AddressOfFunctions[i]]
+		{ &LPCSTR(&imageDosHeader)[AddressOfFunctions[AddressOfNameOrdinals[i]]]
 		, &LPCSTR(&imageDosHeader)[AddressOfNames    [i]]
 		});
 	}
 	std::sort(retval.begin(), retval.end(), [](const ExportedFunction& lhs, const ExportedFunction& rhs) { return lhs.Name() < rhs.Name(); });
 	return retval;
+	}
+	else
+		return {};
 }
